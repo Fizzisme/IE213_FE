b6bfaa6efc8798a416e077da13f5deb4fa6dcbc8
workwithpassion <138347013+workwithpassion@users.noreply.github.com>
2026-03-22 13:28:40 +0700

    update logic authpage, patient page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
-import AuthPage from './components/pages/AuthPage';
-import DemoDashboard from './components/pages/DemoDashboard';
-import PatientInfoForm from './components/pages/PatientInForm';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-    
-      <Routes>
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/demo-dashboard" element={<DemoDashboard />} />
-        <Route path="/create-patient" element={<PatientInfoForm/>} />
-      </Routes>
-    
 import AuthPage from "./components/pages/AuthPage";
 import DemoDashboard from "./components/pages/DemoDashboard";
+import PatientInfoForm from "./components/pages/PatientInForm";
+
 import AdminLogin from "./components/pages/AdminLogin";
 import AdminRegister from "./components/pages/AdminRegister";
 import AdminDashboard from "./components/pages/AdminDashboard";
 import AdminRoute from "./components/guards/AdminRoute";
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+      {/* Auth */}
       <Route path="/" element={<AuthPage />} />
       <Route path="/auth" element={<AuthPage />} />
-      <Route path="/demo-dashboard" element={<DemoDashboard />} />
 
+      {/* User */}
+      <Route path="/demo-dashboard" element={<DemoDashboard />} />
+      <Route path="/create-patient" element={<PatientInfoForm />} />
+
+      {/* Admin */}
       <Route path="/admin-login" element={<AdminLogin />} />
       <Route path="/admin-register" element={<AdminRegister />} />
       <Route
